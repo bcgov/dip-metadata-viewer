67e6593d270eda80b5349b1cd0870a8123347056
Stephanie Hazlitt <stephanie.hazlitt@gov.bc.ca>
2024-03-12 11:12:42 -0700

    update w/ new csv design

--- get-data.R
+++ get-data.R
@@ -10,38 +10,29 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
+
 ## Load libraries
 library(bcdata)
 library(dplyr)
 library(purrr)
-library(janitor)
 library(stringr)
 library(tidyr)
 
 
 ## Function to concatenate all metadata resources (files) for one 
-## BCDC record -- only works with DIP CSV or DIP JSON files
+## BCDC record -- only works with DIP CSV files
 concatenate_all_record_resources <- function(record) {
-  bcdc_record <- bcdc_get_record(record) 
+  bcdc_record <- bcdc_get_record(record)
   
   record_title <- bcdc_record %>%
     pluck("title")
   
-  resources_df <- bcdc_tidy_resources(bcdc_record) |> 
+  resources_df <- bcdc_tidy_resources(bcdc_record) |>
     rename("resource_format" = "format")
   
   d <- map2_dfr(resources_df$id,
                 resources_df$name,
-                ~ if (resources_df[resources_df$id == .x, "ext"] == "json") {
-                  bcdc_get_data(bcdc_record, .x, simplifyVector = TRUE) |>
-                    data.frame() |>
-                    rename_with(~ gsub("fields.", "", .x),
-                                .cols = starts_with("fields")) |>
-                    select(-any_of(c("missingValues", "constraints"))) |>
-                    mutate(bcdc_resource_name = .y,
-                           title = record_title)
-                  
-                } else if (resources_df[resources_df$id == .x, "ext"] == "csv")
+                ~ if (resources_df[resources_df$id == .x, "ext"] == "csv")
                 {
                   bcdc_get_data(bcdc_record, .x,
                                 col_types = readr::cols(.default = "c")) %>%
@@ -50,7 +41,7 @@ concatenate_all_record_resources <- function(record) {
                            title = record_title)
                 }
                 else
-                  (message("Sorry, this resource will not be read since it is not CSV or JSON-formatted")))
+                  (message("Sorry, this resource will not be read since it is not CSV-formatted")))
   
   d2 <- d |>
     left_join(resources_df, by = c("bcdc_resource_name" = "name"))
@@ -58,119 +49,114 @@ concatenate_all_record_resources <- function(record) {
   d2
 }
 
+
 ## Test concatenate_all_record_resources() on one record
-# concatenate_all_record_resources("184b49e0-8e60-4e1b-9886-854db71c1a0e") #json
-# concatenate_all_record_resources("36ad8c2b-a884-44b6-b846-d230673142f2") #csv
-# concatenate_all_record_resources("94b68713-d696-4749-8cc9-bf63ffa09084") #csv+pdf
+# #https://catalogue.data.gov.bc.ca/dataset/metadata-for-community-living-programs---e03
+# concatenate_all_record_resources("659d8cf7-0b60-4944-b93a-0872e2989d4d") #csv
+# #https://catalogue.data.gov.bc.ca/dataset/metadata-for-bc-demographic-survey-e01
+# concatenate_all_record_resources("381445d7-c58b-452e-bf2b-9ad8c6fac499") #csv+pdf
 
 
 ## Data frame of all DIP metadata records in the BCDC
 dip_records_df <- bcdc_list_organization_records("data-innovation-program-dip")
 
-dip_records_active <- dip_records_df |>
-  filter(!str_detect(name, "superseded")) #remove deprecated records
 
 ## Grab and concatenate metadata files for each DIP record into a list
-metadata_by_record <- map(dip_records_active$id,
+metadata_by_record <- map(dip_records_df$id,
                           ~ concatenate_all_record_resources(.x)) |>
-  setNames(dip_records_active$title)
+  setNames(dip_records_df$title)
 
 ## save list to /tmp
-# dir.create("tmp", showWarnings = FALSE)
-# saveRDS(metadata_by_record, "tmp/metadata-list.rds")
+dir.create("tmp", showWarnings = FALSE)
+saveRDS(metadata_by_record, "tmp/metadata-list.rds")
 # metadata_by_record <- readRDS("tmp/metadata-list.rds")
 
-# map(metadata_by_record, ~ colnames(.x))
-# metadata_by_record[[1]]
+
+## Check list
+map(metadata_by_record, ~ colnames(.x))
+metadata_by_record[[5]]
 
 
-## Tidy dip JSON API resources
-json_metadata_list <- metadata_by_record |>
-  keep( ~ ("json" %in% .$ext))
-
-map(json_metadata_list, ~ colnames(.x))
-# json_metadata_list[[1]]
-
-tidy_json_metadata <- function(x) {
-  x |>
-    select(any_of(
-      c(
-        "title",
-        "bcdc_resource_name",
-        "name",
-        "var_class",
-        "type",
-        "description",
-        "url"
-      )
-    ))
-}
-
-# map over JSON API metadata records
-df_metadata_json <- map_dfr(json_metadata_list,
-                            ~ tidy_json_metadata(.x))
-
-
-## Tidy DIP CSV API resources
-csv_metadata_list <- metadata_by_record |> 
-  keep(~("csv" %in% .$ext))
-
-map(csv_metadata_list, ~ colnames(.x))
-
+## Tidy DIP CSV Resources
 tidy_csv_metadata <- function(x) {
   x |>
-    clean_names() |> 
+    filter(!str_detect(bcdc_resource_name, "Codes|Code")) |> 
     select(any_of(
       c(
         "title",
-        "bcdc_resource_name",
-        "name" = "field_name",
-        "var_class" = "variable_classification",
-        "var_class" = "identifier_classification",
-        "type" = "field_type",
-        "description" = "field_description",
-        "description" = "field_description_and_notes",
-        "url"
+        "bcdc_resource_name" = "resources.name",
+        "bcdc_resource_name" = "resources.path",
+        "variable" = "resources.schema.fields.name",
+        "variable_classification" = "resources.schema.fields.var_class",
+        "description" = "resources.schema.fields.description",
+        "bcdc_resource_url" = "url"
       )
     ))
 }
 
+
 # map over CSV API metadata records
-df_metadata_csv <- map_dfr(csv_metadata_list,
-                            ~ tidy_csv_metadata(.x))
-
-## Collapse the csv "code table" rows to 1 per resource (since the attributes are all NA)
-code_files <- df_metadata_csv |> 
-  filter(is.na(name)) |> 
-  group_by(bcdc_resource_name) |> 
-  slice(1L)
-
-code_names <- code_files |>  pull("bcdc_resource_name")
-
-df_metadata_csv_reduced <-
-  df_metadata_csv  |> 
-  filter(!bcdc_resource_name %in% code_names) |> 
-  bind_rows(code_files)
+df_metadata_csv <- map_dfr(metadata_by_record,
+                           ~ tidy_csv_metadata(.x))
 
 
-## Join JSON+CSV tables and final tidying
-tidy_metadata <- df_metadata_csv_reduced |> 
-  bind_rows(df_metadata_json) |> 
+tidy_metadata <- df_metadata_csv |> 
   mutate(
-    var_class = str_replace(var_class, "�", "-"),
+    variable_classification = str_replace(variable_classification, "�", "-"),
     description = str_replace_all(description, "�", " "),
-    bcdc_record_url = str_sub(url, 1, 77)
+    bcdc_record_url = str_sub(bcdc_resource_url, 1, 77)
+  )
+
+
+## save tidy data frame to /data
+dir.create("data", showWarnings = FALSE)
+saveRDS(tidy_metadata, "data/tidy-metadata.rds")
+
+
+
+## Codes only ------------------------------------------------------------------
+
+#all code rows except demographic survey
+tidy_csv_codes <- function(x) {
+  x |>
+    filter(str_detect(bcdc_resource_name, "Codes|Code")) |> 
+    filter(bcdc_resource_name != "Metadata Code Table for BC Demographic Survey") |> 
+  select(any_of(
+    c(
+      "title",
+      "bcdc_resource_name" = "resources.name",
+      "variable" = "resources.schema.fields.name",
+      "variable_classification" = "resources.schema.fields.var_class",
+      "description" = "resources.schema.fields.description",
+      "variable_code_value" = "resources.schema.fields.constraints.enum",
+      "bcdc_resource_url" = "url"
+    )
+  ))
+}
+
+df_codes_csv <- map_dfr(metadata_by_record,
+                           ~ tidy_csv_codes(.x))
+
+tidy_code_data <- df_codes_csv |> 
+  mutate(
+    variable_classification = str_replace(variable_classification, "�", "-"),
+    description = str_replace_all(description, "�", " "),
+    bcdc_record_url = str_sub(bcdc_resource_url, 1, 77)
   ) |> 
   select(
-    title,
-    bcdc_resource_name,
-    name,
-    var_class,
-    type,
-    description,
-    bcdc_record_url
+    "title",
+    "bcdc_resource_name",
+    "variable",
+    "variable_classification",
+    "description",
+    "variable_code_value",
+    "bcdc_resource_url",
+    "bcdc_record_url"
   )
-  
+
+survey <- metadata_by_record[["Metadata for BC Demographic Survey - E01"]] |> 
+  filter(str_detect(bcdc_resource_name, "Codes|Code"))
+
 ## save tidy data frame to /data
 dir.create("data", showWarnings = FALSE)
 saveRDS(tidy_metadata, "data/tidy-metadata.rds")
